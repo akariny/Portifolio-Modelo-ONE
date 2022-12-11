ad7c02c4dc62118826080c9fdbad909ed747aa50
Ana Kariny <anakariny10@gmail.com>
2022-12-11 01:59:48 -0300

    finalizando portifolio

---
+++ js/formulario.js
@@ -0,0 +1,43 @@
+const nome = document.querySelector('[data-nome]');
+const email = document.querySelector('[data-email]');
+const mensagem = document.querySelector('[data-mensagem]');
+
+function validaFormulario() {
+    validaNome();
+    validaEmail();
+    validaMensagem();
+}
+
+const validaNome = () => {
+    const erroNome = document.querySelector('[data-erro-nome]');
+
+    if(nome.value === '') {
+        erroNome.innerHTML = 'O campo nome não pode estar vazio';
+    } else {
+        erroNome.innerHTML = '';
+    }
+}
+
+const validaEmail = () => {
+    const erroEmail = document.querySelector('[data-erro-email]');
+
+    if(email.value === '') {
+        erroEmail.innerHTML = 'O campo email não pode estar vazio';
+    } else {
+        if(email.value.indexOf('@') === -1) {
+            erroEmail.innerHTML = 'O email digitado não é valido'
+        } else {
+            erroEmail.innerHTML = '';
+        }
+    }
+}
+
+const validaMensagem = () => {
+    const erroMensagem = document.querySelector('[data-erro-mensagem]');
+
+    if(mensagem.value === '') {
+        erroMensagem.innerHTML = 'Escreva a sua mensagem :)';
+    } else {
+        erroMensagem.innerHTML = '';
+    }
+}
